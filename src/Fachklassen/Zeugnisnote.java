be0b72d85cf5f17111b56771d5d476b1e5691a8d
Danny <lemke-danny@t-online.de>
2016-06-29 10:33:24 +0200

    Upload Zeugnisnote

--- src/Fachklassen/Zeugnisnote.java
+++ src/Fachklassen/Zeugnisnote.java
@@ -4,135 +4,49 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import javax.persistence.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-<<<<<<< HEAD
-=======
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
->>>>>>> refs/remotes/origin/master
 
 import Persistenz.DBZugriff;
-
-
 
 @Entity
 public class Zeugnisnote 
 {
-	// Konstruktor
-	//---------------------------------------------------------------------------------------------
-	public Zeugnisnote()
-	{
-		
-	}
-	
-	public Zeugnisnote(int noteZeugnis, int noteErrechnet, String bemerkung, LocalDate aenderungszeitpunkt)
-	{
-		this.noteZeugnis = noteZeugnis;
-		this.noteErrechnet = noteErrechnet;
-		this.bemerkung = bemerkung;
-		this.aenderungszeitpunkt = aenderungszeitpunkt;
-	}
-	//---------------------------------------------------------------------------------------------
-	
-	// Instanzvariablen
-	//---------------------------------------------------------------------------------------------
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private int id;
-	
-	private int noteZeugnis;
-	
+	private int noteZeugis;
 	private double noteErrechnet;
-	
 	private String bemerkung;
 	
+	@OneToOne		//nict sicht ob richtig
+	private Zeugnisfach zeugnisfach;
+	
+	@ManyToOne
+	private Schueler schueler;
+	
 	private LocalDate aenderungszeitpunkt;
-	//---------------------------------------------------------------------------------------------
-
 	
-	// Instanzvariablen -> Fremdschlüssel
-	//---------------------------------------------------------------------------------------------
-	// private int idZeugnisfach;
-	// private int idSchueler;
-	// private int idZeugnis;
-	//---------------------------------------------------------------------------------------------
+	@ManyToOne
+	private Zeugnis zeugnis;
 	
+	//------------------------------------------------------
 	
-	// Getter / Setter
-	//---------------------------------------------------------------------------------------------
-	public int getId() 
-	{
-		return id;
-	}
-	public void setId(int id) 
-	{
-		this.id = id;
-	}
-	
-	public int getNoteZeugnis() 
-	{
-		return noteZeugnis;
-	}
-	public void setNoteZeugnis(int noteZeugnis) 
-	{
-		this.noteZeugnis = noteZeugnis;
-	}
-	
-	public double getNoteErrechnet() 
-	{
-		return noteErrechnet;
-	}
-	public void setNoteErrechnet(double noteErrechnet) 
-	{
-		this.noteErrechnet = noteErrechnet;
-	}
-	
-	public String getBemerkung() 
-	{
-		return bemerkung;
-	}
-	public void setBemerkung(String bemerkung) 
-	{
-		this.bemerkung = bemerkung;
-	}
-	
-	public LocalDate getAenderungszeitpunkt() 
-	{
-		return aenderungszeitpunkt;
-	}
-	public void setAenderungszeitpunkt(LocalDate aenderungszeitpunkt) 
-	{
-		this.aenderungszeitpunkt = aenderungszeitpunkt;
-	}
-	//---------------------------------------------------------------------------------------------
-	
-	
-	// Methoden
-	//---------------------------------------------------------------------------------------------
-	public void lesen(int id)
-	{
-		DBZugriff.lesen(this, id);
-	}
-	public void loeschen()
-	{
-		DBZugriff.loeschen(this);
-	}
-	public String toString()
-	{
-		return "";
-	}
 	public boolean speichern()
 	{
 		return DBZugriff.speichern(this);
 	}
+	
 	public double berechneNote(Unterrichtsfach uf)
 	{
 		double muendlich = 0;
 		double schriftlich = 0;
 		double note = 0;
 		double i = 0;
-		
-		ArrayList<Leistung>leistungen = new ArrayList<Leistung>(Leistung.AlleLesen(schueler, uf));
+		ArrayList<Leistung>leistungen = new ArrayList<>(Leistung.AlleLesen(schueler, uf));
 		for (Leistung l : leistungen )
 		{
 			if(l.getLeistungsart().getBez() == "Schulaufgabe")
@@ -149,11 +63,69 @@ public class Zeugnisnote
 		
 		return note;
 	}
-	//---------------------------------------------------------------------------------------------
 	
 	
-	// Methoden -> Static
-	//---------------------------------------------------------------------------------------------
+	//-----------------------------------
 	
-	//---------------------------------------------------------------------------------------------
+	public int getNoteZeugis() {
+		return noteZeugis;
+	}
+
+	public void setNoteZeugis(int noteZeugis) {
+		this.noteZeugis = noteZeugis;
+	}
+
+	public double getNoteErrechnet() {
+		return noteErrechnet;
+	}
+
+	public void setNoteErrechnet(double noteErrechnet) {
+		this.noteErrechnet = noteErrechnet;
+	}
+
+	public String getBemerkung() {
+		return bemerkung;
+	}
+
+	public void setBemerkung(String bemerkung) {
+		this.bemerkung = bemerkung;
+	}
+
+	public Zeugnisfach getZeugnisfach() {
+		return zeugnisfach;
+	}
+
+	public void setZeugnisfach(Zeugnisfach zeugnisfach) {
+		this.zeugnisfach = zeugnisfach;
+	}
+
+	public Schueler getSchueler() {
+		return schueler;
+	}
+
+	public void setSchueler(Schueler schueler) {
+		this.schueler = schueler;
+	}
+
+	public LocalDate getAenderungszeitpunkt() {
+		return aenderungszeitpunkt;
+	}
+
+	public void setAenderungszeitpunkt(LocalDate aenderungszeitpunkt) {
+		this.aenderungszeitpunkt = aenderungszeitpunkt;
+	}
+
+	public Zeugnis getZeugnis() {
+		return zeugnis;
+	}
+
+	public void setZeugnis(Zeugnis zeugnis) {
+		this.zeugnis = zeugnis;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	
 }
