d34614fda5adf06ff44349eb9d63d5f68d7aa4e0
Christoph Heider <Christoph Heider>
2016-07-05 11:43:53 +0200

    Redesign von Dialog_NotenausgabeKlasse für erstes Halbjahr abgeschlossen, Klassen NotenTable und NotenTableHeader implementiert um Customtooltips zu ermöglich, diese sind nicht zu mappen

---
+++ src/Dialog/NotenTable.java
@@ -0,0 +1,23 @@
+package Dialog;
+
+import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+
+public class NotenTable extends JTable
+{
+
+	@Override
+	public void addColumn(TableColumn aColumn)
+	{
+		// TODO Auto-generated method stub
+		super.addColumn(aColumn);
+	}
+
+	@Override
+	protected JTableHeader createDefaultTableHeader()
+	{
+		return new NotenTableHeader(columnModel);
+	}
+	
+}
