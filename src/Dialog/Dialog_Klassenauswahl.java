338397ff5aaf62cf7a841326c8f72e651241f93c
Danny <lemke-danny@t-online.de>
2016-07-06 13:08:01 +0200

    GUI Klassenauswahl neu Designed

--- src/Dialog/Dialog_Klassenauswahl.java
+++ src/Dialog/Dialog_Klassenauswahl.java
@@ -1,53 +1,58 @@
 package Dialog;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
 import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 
 import java.awt.Font;
+import java.util.ArrayList;
 
 import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
-import Fachklassen.*;
-import Persistenz.DBZugriff;
-
+import Fachklassen.Klasse;
+import Fachklassen.Lehrer;
+import Fachklassen.Schule;
+import Fachklassen.Unterrichtsfach;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.Dimension;
-import java.util.ArrayList;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
 
-
 public class Dialog_Klassenauswahl extends JFrame implements ActionListener, ItemListener
+
 {
-
-	private final JPanel contentPanel = new JPanel();
-
-	JComboBox<Klasse> comboBox_Klassen;
-	JComboBox<Unterrichtsfach> comboBox_Faecher;
-	
-	JButton btnbersicht;
-	JButton button_Zurueck;
-	
-	Klasse klasse = new Klasse();
+	private JPanel panel_combobox;
+	private JPanel panel_button;
+	private JPanel panel_klassenleiter;
 	private JLabel lblSchuleauswahl;
-	private JComboBox<Schule> comboBox_Schule;
+	private JLabel lblKlassenauswahl;
+	private JComboBox<Schule> comboBox_Schule;	
+	private JComboBox<Klasse> comboBox_Klassen;
+	private JComboBox<Unterrichtsfach> comboBox_Faecher;
+	private JLabel lblFcherauswahl;
+	private JButton btnFachuebersicht;
+	private JButton btnKlassenuebersicht;
+	private JButton btnZurueck;
+	private JLabel lblKlassenleiter;
+	private JSeparator separator;
+	private JSeparator separator_1;
+	private JButton btnZeugnisnotenAnlegen;
 	private Lehrer lehrer;
-	private JButton button_klsicht;
-	private JButton button_zurueck;
+	Klasse klasse = new Klasse();
+
 	/**
 	 * Create the dialog.
 	 */
+	
 	public Dialog_Klassenauswahl(Lehrer lehrer)
 	{
 		this.lehrer = lehrer;	
@@ -58,193 +63,211 @@ public class Dialog_Klassenauswahl extends JFrame implements ActionListener, Ite
 		{
 			if(k.getIdKlassenleiter().getId() == this.lehrer.getId())
 			{
-				this.button_klsicht.setEnabled(true);
+				this.btnKlassenuebersicht.setEnabled(true);
+				this.btnZeugnisnotenAnlegen.setEnabled(true);
 				found = true;
 			}
 			else if (k.getIdKlassenleiter().getId() != this.lehrer.getId() && found == false)
 			{
-				this.button_klsicht.setEnabled(false);
+				this.btnKlassenuebersicht.setEnabled(false);
+				this.btnZeugnisnotenAnlegen.setEnabled(false);
 			}
 		}
 		SetDatenInMaske();
 	}
-	private void initGUI() {
-		
-		setMinimumSize(new Dimension(450, 300));
-		setTitle("Klassenauswahl");
-		setBounds(100, 100, 450, 320);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{434, 0};
-		gridBagLayout.rowHeights = new int[]{261, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		getContentPane().setLayout(gridBagLayout);
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		GridBagConstraints gbc_contentPanel = new GridBagConstraints();
-		gbc_contentPanel.fill = GridBagConstraints.BOTH;
-		gbc_contentPanel.gridx = 0;
-		gbc_contentPanel.gridy = 0;
-		getContentPane().add(contentPanel, gbc_contentPanel);
-		GridBagLayout gbl_contentPanel = new GridBagLayout();
-		gbl_contentPanel.columnWidths = new int[]{25, 350, 25, 0};
-		gbl_contentPanel.rowHeights = new int[]{25, 150, 50, 25, 0};
-		gbl_contentPanel.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPanel.rowWeights = new double[]{1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		contentPanel.setLayout(gbl_contentPanel);
-		{
-			JPanel panel = new JPanel();
-			GridBagConstraints gbc_panel = new GridBagConstraints();
-			gbc_panel.insets = new Insets(0, 0, 5, 5);
-			gbc_panel.fill = GridBagConstraints.BOTH;
-			gbc_panel.gridx = 1;
-			gbc_panel.gridy = 1;
-			contentPanel.add(panel, gbc_panel);
-			GridBagLayout gbl_panel = new GridBagLayout();
-			gbl_panel.columnWidths = new int[]{0, 0};
-			gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
-			gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-			gbl_panel.rowWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-			panel.setLayout(gbl_panel);
-			{
-				lblSchuleauswahl = new JLabel("Schule (Auswahl)");
-				lblSchuleauswahl.setFont(new Font("Tahoma", Font.PLAIN, 16));
-				GridBagConstraints gbc_lblSchuleauswahl = new GridBagConstraints();
-				gbc_lblSchuleauswahl.insets = new Insets(0, 0, 5, 0);
-				gbc_lblSchuleauswahl.gridx = 0;
-				gbc_lblSchuleauswahl.gridy = 0;
-				panel.add(lblSchuleauswahl, gbc_lblSchuleauswahl);
-			}
-			{
-				comboBox_Schule = new JComboBox();
-				this.comboBox_Schule.addItemListener(this);
-				GridBagConstraints gbc_comboBox_Schule = new GridBagConstraints();
-				gbc_comboBox_Schule.ipady = 10;
-				gbc_comboBox_Schule.anchor = GridBagConstraints.NORTH;
-				gbc_comboBox_Schule.insets = new Insets(0, 0, 5, 0);
-				gbc_comboBox_Schule.fill = GridBagConstraints.HORIZONTAL;
-				gbc_comboBox_Schule.gridx = 0;
-				gbc_comboBox_Schule.gridy = 1;
-				panel.add(comboBox_Schule, gbc_comboBox_Schule);
-			}
-			{
-				JLabel lblKlassenauswahl = new JLabel("Klassen (Auswahl)");
-				lblKlassenauswahl.setHorizontalAlignment(SwingConstants.CENTER);
-				lblKlassenauswahl.setVerticalAlignment(SwingConstants.BOTTOM);
-				lblKlassenauswahl.setFont(new Font("Tahoma", Font.PLAIN, 16));
-				GridBagConstraints gbc_lblKlassenauswahl = new GridBagConstraints();
-				gbc_lblKlassenauswahl.insets = new Insets(0, 0, 5, 0);
-				gbc_lblKlassenauswahl.gridx = 0;
-				gbc_lblKlassenauswahl.gridy = 2;
-				panel.add(lblKlassenauswahl, gbc_lblKlassenauswahl);
-			}
-			{
-				comboBox_Klassen = new JComboBox();
-				comboBox_Klassen.setEnabled(false);
-				GridBagConstraints gbc_comboBox_Klassen = new GridBagConstraints();
-				gbc_comboBox_Klassen.fill = GridBagConstraints.HORIZONTAL;
-				gbc_comboBox_Klassen.anchor = GridBagConstraints.NORTH;
-				gbc_comboBox_Klassen.ipady = 10;
-				gbc_comboBox_Klassen.insets = new Insets(0, 0, 5, 0);
-				gbc_comboBox_Klassen.gridx = 0;
-				gbc_comboBox_Klassen.gridy = 3;
-				panel.add(comboBox_Klassen, gbc_comboBox_Klassen);
-			}
-			{
-				JLabel lblFcherauswahl = new JLabel("F\u00E4cher (Auswahl)");
-				lblFcherauswahl.setHorizontalAlignment(SwingConstants.CENTER);
-				lblFcherauswahl.setVerticalAlignment(SwingConstants.BOTTOM);
-				lblFcherauswahl.setFont(new Font("Tahoma", Font.PLAIN, 16));
-				GridBagConstraints gbc_lblFcherauswahl = new GridBagConstraints();
-				gbc_lblFcherauswahl.insets = new Insets(0, 0, 5, 0);
-				gbc_lblFcherauswahl.gridx = 0;
-				gbc_lblFcherauswahl.gridy = 4;
-				panel.add(lblFcherauswahl, gbc_lblFcherauswahl);
-			}
-			{
-				comboBox_Faecher = new JComboBox();
-				comboBox_Faecher.setEnabled(false);
-				GridBagConstraints gbc_comboBox_Faecher = new GridBagConstraints();
-				gbc_comboBox_Faecher.anchor = GridBagConstraints.NORTH;
-				gbc_comboBox_Faecher.ipady = 10;
-				gbc_comboBox_Faecher.fill = GridBagConstraints.HORIZONTAL;
-				gbc_comboBox_Faecher.gridx = 0;
-				gbc_comboBox_Faecher.gridy = 5;
-				panel.add(comboBox_Faecher, gbc_comboBox_Faecher);
-			}
-		}
-		{
-			JPanel panel = new JPanel();
-			GridBagConstraints gbc_panel = new GridBagConstraints();
-			gbc_panel.insets = new Insets(0, 0, 5, 5);
-			gbc_panel.fill = GridBagConstraints.BOTH;
-			gbc_panel.gridx = 1;
-			gbc_panel.gridy = 2;
-			contentPanel.add(panel, gbc_panel);
-			GridBagLayout gbl_panel = new GridBagLayout();
-			gbl_panel.columnWidths = new int[]{0, 0, 0, 0};
-			gbl_panel.rowHeights = new int[]{0, 0};
-			gbl_panel.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
-			gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-			panel.setLayout(gbl_panel);
-			{
-				btnbersicht = new JButton("Fach\u00FCbersicht");
-				this.btnbersicht.setEnabled(false);
-				btnbersicht.addActionListener(this);
-				GridBagConstraints gbc_btnbersicht = new GridBagConstraints();
-				gbc_btnbersicht.fill = GridBagConstraints.HORIZONTAL;
-				gbc_btnbersicht.insets = new Insets(0, 0, 0, 5);
-				gbc_btnbersicht.gridx = 0;
-				gbc_btnbersicht.gridy = 0;
-				panel.add(btnbersicht, gbc_btnbersicht);
-			}
-			{
-				{
-					this.button_klsicht = new JButton("Klassen\u00FCbersicht");
-					this.button_klsicht.addActionListener(this);
-					GridBagConstraints gbc_button_klsicht = new GridBagConstraints();
-					gbc_button_klsicht.fill = GridBagConstraints.HORIZONTAL;
-					gbc_button_klsicht.insets = new Insets(0, 0, 0, 5);
-					gbc_button_klsicht.gridx = 1;
-					gbc_button_klsicht.gridy = 0;
-					panel.add(this.button_klsicht, gbc_button_klsicht);
-				}
-			}
-			{
-				this.button_zurueck = new JButton("Zur\u00FCck");
-				this.button_zurueck.addActionListener(this);
-				GridBagConstraints gbc_button_zurueck = new GridBagConstraints();
-				gbc_button_zurueck.gridx = 2;
-				gbc_button_zurueck.gridy = 0;
-				panel.add(this.button_zurueck, gbc_button_zurueck);
-			}
-		}
-	}
-
+	
 	public void SetDatenInMaske()
 	{			
 		//Combobox für Schule füllen
 		for(Schule s :Schule.alleLesen() )
 		{
 			comboBox_Schule.addItem(s);	
-		}
-						
+		}						
 	}
 	
+	private void initGUI() 
+	{
+		setResizable(false);
+		setBounds(100, 100, 525, 300);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{340, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{216, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+		
+		this.panel_combobox = new JPanel();
+		GridBagConstraints gbc_panel_combobox = new GridBagConstraints();
+		gbc_panel_combobox.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_combobox.fill = GridBagConstraints.BOTH;
+		gbc_panel_combobox.gridx = 0;
+		gbc_panel_combobox.gridy = 0;
+		getContentPane().add(this.panel_combobox, gbc_panel_combobox);
+		GridBagLayout gbl_panel_combobox = new GridBagLayout();
+		gbl_panel_combobox.columnWidths = new int[]{0, 0};
+		gbl_panel_combobox.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_combobox.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_combobox.rowWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		this.panel_combobox.setLayout(gbl_panel_combobox);
+		
+		this.lblSchuleauswahl = new JLabel("Schule");
+		this.lblSchuleauswahl.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_lblSchuleauswahl = new GridBagConstraints();
+		gbc_lblSchuleauswahl.insets = new Insets(5, 0, 5, 0);
+		gbc_lblSchuleauswahl.gridx = 0;
+		gbc_lblSchuleauswahl.gridy = 0;
+		this.panel_combobox.add(this.lblSchuleauswahl, gbc_lblSchuleauswahl);
+		
+		this.comboBox_Schule = new JComboBox();
+		this.comboBox_Schule.addItemListener(this);
+		GridBagConstraints gbc_comboBox_Schule = new GridBagConstraints();
+		gbc_comboBox_Schule.insets = new Insets(5, 10, 5, 10);
+		gbc_comboBox_Schule.fill = GridBagConstraints.BOTH;
+		gbc_comboBox_Schule.gridx = 0;
+		gbc_comboBox_Schule.gridy = 1;
+		this.panel_combobox.add(this.comboBox_Schule, gbc_comboBox_Schule);
+		
+		this.lblKlassenauswahl = new JLabel("Klassen");
+		this.lblKlassenauswahl.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_lblKlassenauswahl = new GridBagConstraints();
+		gbc_lblKlassenauswahl.insets = new Insets(0, 0, 5, 0);
+		gbc_lblKlassenauswahl.gridx = 0;
+		gbc_lblKlassenauswahl.gridy = 2;
+		this.panel_combobox.add(this.lblKlassenauswahl, gbc_lblKlassenauswahl);
+		
+		this.comboBox_Klassen = new JComboBox();
+		this.comboBox_Klassen.setEnabled(false);
+		GridBagConstraints gbc_comboBox_Klassen = new GridBagConstraints();
+		gbc_comboBox_Klassen.insets = new Insets(5, 10, 5, 10);
+		gbc_comboBox_Klassen.fill = GridBagConstraints.BOTH;
+		gbc_comboBox_Klassen.gridx = 0;
+		gbc_comboBox_Klassen.gridy = 3;
+		this.panel_combobox.add(this.comboBox_Klassen, gbc_comboBox_Klassen);
+		
+		this.lblFcherauswahl = new JLabel("F\u00E4cher");
+		this.lblFcherauswahl.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_lblFcherauswahl = new GridBagConstraints();
+		gbc_lblFcherauswahl.insets = new Insets(0, 0, 5, 0);
+		gbc_lblFcherauswahl.gridx = 0;
+		gbc_lblFcherauswahl.gridy = 4;
+		this.panel_combobox.add(this.lblFcherauswahl, gbc_lblFcherauswahl);
+		
+		this.comboBox_Faecher = new JComboBox();
+		this.comboBox_Faecher.setEnabled(false);
+		this.comboBox_Faecher.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		GridBagConstraints gbc_comboBox_Faecher = new GridBagConstraints();
+		gbc_comboBox_Faecher.insets = new Insets(5, 10, 5, 10);
+		gbc_comboBox_Faecher.fill = GridBagConstraints.BOTH;
+		gbc_comboBox_Faecher.gridx = 0;
+		gbc_comboBox_Faecher.gridy = 5;
+		this.panel_combobox.add(this.comboBox_Faecher, gbc_comboBox_Faecher);
+		
+		this.separator_1 = new JSeparator();
+		this.separator_1.setOrientation(SwingConstants.VERTICAL);
+		GridBagConstraints gbc_separator_1 = new GridBagConstraints();
+		gbc_separator_1.gridheight = 2;
+		gbc_separator_1.fill = GridBagConstraints.BOTH;
+		gbc_separator_1.gridx = 1;
+		gbc_separator_1.gridy = 0;
+		getContentPane().add(this.separator_1, gbc_separator_1);
+		
+		this.panel_klassenleiter = new JPanel();
+		GridBagConstraints gbc_panel_klassenleiter = new GridBagConstraints();
+		gbc_panel_klassenleiter.gridheight = 2;
+		gbc_panel_klassenleiter.fill = GridBagConstraints.BOTH;
+		gbc_panel_klassenleiter.gridx = 2;
+		gbc_panel_klassenleiter.gridy = 0;
+		getContentPane().add(this.panel_klassenleiter, gbc_panel_klassenleiter);
+		GridBagLayout gbl_panel_klassenleiter = new GridBagLayout();
+		gbl_panel_klassenleiter.columnWidths = new int[]{0, 0};
+		gbl_panel_klassenleiter.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_panel_klassenleiter.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_klassenleiter.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		this.panel_klassenleiter.setLayout(gbl_panel_klassenleiter);
+		
+		this.lblKlassenleiter = new JLabel("Klassenleiter");
+		this.lblKlassenleiter.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_lblKlassenleiter = new GridBagConstraints();
+		gbc_lblKlassenleiter.insets = new Insets(5, 10, 5, 10);
+		gbc_lblKlassenleiter.gridx = 0;
+		gbc_lblKlassenleiter.gridy = 0;
+		this.panel_klassenleiter.add(this.lblKlassenleiter, gbc_lblKlassenleiter);
+		
+		this.separator = new JSeparator();
+		GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.insets = new Insets(0, 0, 10, 0);
+		gbc_separator.fill = GridBagConstraints.BOTH;
+		gbc_separator.gridx = 0;
+		gbc_separator.gridy = 1;
+		this.panel_klassenleiter.add(this.separator, gbc_separator);
+		
+		this.btnKlassenuebersicht = new JButton("Klassen\u00FCbersicht");
+		this.btnKlassenuebersicht.addActionListener(this);
+		this.btnKlassenuebersicht.setEnabled(false);
+		GridBagConstraints gbc_btnKlassenuebersicht = new GridBagConstraints();
+		gbc_btnKlassenuebersicht.insets = new Insets(5, 5, 5, 5);
+		gbc_btnKlassenuebersicht.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnKlassenuebersicht.gridx = 0;
+		gbc_btnKlassenuebersicht.gridy = 2;
+		this.panel_klassenleiter.add(this.btnKlassenuebersicht, gbc_btnKlassenuebersicht);
+		
+		this.btnZeugnisnotenAnlegen = new JButton("Zeugnisnoten anlegen");
+		this.btnZeugnisnotenAnlegen.addActionListener(this);
+		this.btnZeugnisnotenAnlegen.setEnabled(false);
+		GridBagConstraints gbc_btnZeugnisnotenAnlegen = new GridBagConstraints();
+		gbc_btnZeugnisnotenAnlegen.insets = new Insets(5, 5, 5, 5);
+		gbc_btnZeugnisnotenAnlegen.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnZeugnisnotenAnlegen.gridx = 0;
+		gbc_btnZeugnisnotenAnlegen.gridy = 3;
+		this.panel_klassenleiter.add(this.btnZeugnisnotenAnlegen, gbc_btnZeugnisnotenAnlegen);
+		
+		this.panel_button = new JPanel();
+		GridBagConstraints gbc_panel_button = new GridBagConstraints();
+		gbc_panel_button.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_button.fill = GridBagConstraints.BOTH;
+		gbc_panel_button.gridx = 0;
+		gbc_panel_button.gridy = 1;
+		getContentPane().add(this.panel_button, gbc_panel_button);
+		GridBagLayout gbl_panel_button = new GridBagLayout();
+		gbl_panel_button.columnWidths = new int[]{0, 0, 0};
+		gbl_panel_button.rowHeights = new int[]{0, 0};
+		gbl_panel_button.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_button.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		this.panel_button.setLayout(gbl_panel_button);
+		
+		this.btnFachuebersicht = new JButton("Fach\u00FCbersicht");
+		this.btnFachuebersicht.addActionListener(this);
+		GridBagConstraints gbc_btnFachuebersicht = new GridBagConstraints();
+		gbc_btnFachuebersicht.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnFachuebersicht.insets = new Insets(0, 10, 0, 10);
+		gbc_btnFachuebersicht.gridx = 0;
+		gbc_btnFachuebersicht.gridy = 0;
+		this.panel_button.add(this.btnFachuebersicht, gbc_btnFachuebersicht);
+		
+		this.btnZurueck = new JButton("Zur\u00FCck");
+		this.btnZurueck.addActionListener(this);
+		GridBagConstraints gbc_btnZurueck = new GridBagConstraints();
+		gbc_btnZurueck.insets = new Insets(0, 10, 0, 10);
+		gbc_btnZurueck.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnZurueck.gridx = 1;
+		gbc_btnZurueck.gridy = 0;
+		this.panel_button.add(this.btnZurueck, gbc_btnZurueck);
+	}
 	
 	public void actionPerformed(ActionEvent e) 
 	{
-		if(e.getActionCommand().equals(btnbersicht.getActionCommand())) // Abfrage auf Drücken des Login-Buttons "button_Login"
+		if(e.getActionCommand().equals(btnFachuebersicht.getActionCommand())) // Abfrage auf Drücken des Login-Buttons "button_Login"
 		{
 			Dialog_NotenausgabeKlasse dlg_notenausgabeKlasse = new Dialog_NotenausgabeKlasse(lehrer,(Klasse)comboBox_Klassen.getSelectedItem(),(Unterrichtsfach)comboBox_Faecher.getSelectedItem());
 			dlg_notenausgabeKlasse.setVisible(true);
 		}		
-		if(e.getActionCommand().equals(button_zurueck.getActionCommand())) // Abfrage auf Drücken des Login-Buttons "button_Login"
+		if(e.getActionCommand().equals(btnZurueck.getActionCommand())) // Abfrage auf Drücken des Login-Buttons "button_Login"
 		{
 			this.dispose();
 			Dialog_Login dlg_login = new Dialog_Login();
 			dlg_login.setVisible(true);
 		}
-		if(e.getActionCommand().equals(button_klsicht.getActionCommand())) // Abfrage ob 
+		if(e.getActionCommand().equals(btnKlassenuebersicht.getActionCommand())) // Abfrage ob 
 		{
 			
 		}
@@ -254,7 +277,7 @@ public class Dialog_Klassenauswahl extends JFrame implements ActionListener, Ite
 		comboBox_Faecher.removeAllItems();
 		comboBox_Klassen.removeAllItems();
 		comboBox_Klassen.setEnabled(true);
-		btnbersicht.setEnabled(false);
+		btnFachuebersicht.setEnabled(false);
 		
 		for(Klasse k:Klasse.AlleLesen(lehrer, (Schule)comboBox_Schule.getSelectedItem()))
 		{
@@ -271,8 +294,7 @@ public class Dialog_Klassenauswahl extends JFrame implements ActionListener, Ite
 		}
 		if(comboBox_Faecher.getSelectedItem() != null)
 		{
-			btnbersicht.setEnabled(true);
+			btnFachuebersicht.setEnabled(true);
 		}
-		
 	}
 }
