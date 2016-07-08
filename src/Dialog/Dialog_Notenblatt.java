57b58a71cfc0a94b052aca7173308aa99e161d7f
Tim <tim-stroessner@t-online.de>
2016-07-08 09:59:52 +0200

    Notenblatt Update Schuelerwahl Update

--- src/Dialog/Dialog_Notenblatt.java
+++ src/Dialog/Dialog_Notenblatt.java
@@ -1,61 +1,60 @@
 package Dialog;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.CardLayout;
+
+
+
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JTable;
 import javax.swing.border.LineBorder;
-import java.awt.Color;
+import javax.swing.table.DefaultTableModel;
 
-public class Dialog_Notenblatt extends JDialog {
+import Fachklassen.Schueler;
+
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Dialog_Notenblatt extends JFrame {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTable table;
+	private JTextField txt_schueler;
+	private JTextField txt_klasse;
+	private JTextField txt_lehrer;
+	Schueler schueler;
+	private JTable notentabelle;
+	private DefaultTableModel model = new DefaultTableModel();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			Dialog_Notenblatt dialog = new Dialog_Notenblatt();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Create the dialog.
-	 */
-	public Dialog_Notenblatt() {
+	public Dialog_Notenblatt(Schueler schueler) 
+	{
+		this.schueler = schueler;
 		setTitle("Notenblatt");
 		initGUI();
+		this.setDatenInMaske();	
 	}
 	private void initGUI() {
-		setModal(true);
+		this.setExtendedState(MAXIMIZED_BOTH);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_contentPanel = new GridBagLayout();
 		gbl_contentPanel.columnWidths = new int[]{25, 424, 25, 0};
-		gbl_contentPanel.rowHeights = new int[]{25, 0, 0, 0, 25, 0};
+		gbl_contentPanel.rowHeights = new int[]{25, 0, 0, 0, 0};
 		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		contentPanel.setLayout(gbl_contentPanel);
 		{
 			JPanel panel = new JPanel();
@@ -94,15 +93,15 @@ public class Dialog_Notenblatt extends JDialog {
 					panel_1.add(lblSchlerin, gbc_lblSchlerin);
 				}
 				{
-					textField = new JTextField();
-					textField.setEditable(false);
-					GridBagConstraints gbc_textField = new GridBagConstraints();
-					gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-					gbc_textField.insets = new Insets(0, 0, 5, 0);
-					gbc_textField.gridx = 1;
-					gbc_textField.gridy = 0;
-					panel_1.add(textField, gbc_textField);
-					textField.setColumns(10);
+					txt_schueler = new JTextField();
+					txt_schueler.setEditable(false);
+					GridBagConstraints gbc_txt_schueler = new GridBagConstraints();
+					gbc_txt_schueler.fill = GridBagConstraints.HORIZONTAL;
+					gbc_txt_schueler.insets = new Insets(0, 0, 5, 0);
+					gbc_txt_schueler.gridx = 1;
+					gbc_txt_schueler.gridy = 0;
+					panel_1.add(txt_schueler, gbc_txt_schueler);
+					txt_schueler.setColumns(10);
 				}
 				{
 					JLabel lblKlasse = new JLabel("Klasse");
@@ -113,14 +112,14 @@ public class Dialog_Notenblatt extends JDialog {
 					panel_1.add(lblKlasse, gbc_lblKlasse);
 				}
 				{
-					textField_1 = new JTextField();
-					textField_1.setEditable(false);
-					GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-					gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-					gbc_textField_1.gridx = 1;
-					gbc_textField_1.gridy = 1;
-					panel_1.add(textField_1, gbc_textField_1);
-					textField_1.setColumns(10);
+					txt_klasse = new JTextField();
+					txt_klasse.setEditable(false);
+					GridBagConstraints gbc_txt_klasse = new GridBagConstraints();
+					gbc_txt_klasse.fill = GridBagConstraints.HORIZONTAL;
+					gbc_txt_klasse.gridx = 1;
+					gbc_txt_klasse.gridy = 1;
+					panel_1.add(txt_klasse, gbc_txt_klasse);
+					txt_klasse.setColumns(10);
 				}
 			}
 			{
@@ -137,7 +136,7 @@ public class Dialog_Notenblatt extends JDialog {
 				gbl_panel_1.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 				panel_1.setLayout(gbl_panel_1);
 				{
-					JLabel lblLehrer = new JLabel("Lehrer");
+					JLabel lblLehrer = new JLabel("Klassenleiter");
 					GridBagConstraints gbc_lblLehrer = new GridBagConstraints();
 					gbc_lblLehrer.insets = new Insets(0, 0, 0, 5);
 					gbc_lblLehrer.anchor = GridBagConstraints.EAST;
@@ -146,14 +145,14 @@ public class Dialog_Notenblatt extends JDialog {
 					panel_1.add(lblLehrer, gbc_lblLehrer);
 				}
 				{
-					textField_2 = new JTextField();
-					textField_2.setEditable(false);
-					GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-					gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-					gbc_textField_2.gridx = 1;
-					gbc_textField_2.gridy = 0;
-					panel_1.add(textField_2, gbc_textField_2);
-					textField_2.setColumns(10);
+					txt_lehrer = new JTextField();
+					txt_lehrer.setEditable(false);
+					GridBagConstraints gbc_txt_lehrer = new GridBagConstraints();
+					gbc_txt_lehrer.fill = GridBagConstraints.HORIZONTAL;
+					gbc_txt_lehrer.gridx = 1;
+					gbc_txt_lehrer.gridy = 0;
+					panel_1.add(txt_lehrer, gbc_txt_lehrer);
+					txt_lehrer.setColumns(10);
 				}
 			}
 		}
@@ -172,38 +171,71 @@ public class Dialog_Notenblatt extends JDialog {
 			gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 			panel.setLayout(gbl_panel);
 			{
-				table = new JTable();
-				table.setBorder(new LineBorder(Color.GRAY));
-				GridBagConstraints gbc_table = new GridBagConstraints();
-				gbc_table.fill = GridBagConstraints.BOTH;
-				gbc_table.gridx = 0;
-				gbc_table.gridy = 0;
-				panel.add(table, gbc_table);
+				JScrollPane scrollPane = new JScrollPane();
+				GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+				gbc_scrollPane.fill = GridBagConstraints.BOTH;
+				gbc_scrollPane.gridx = 0;
+				gbc_scrollPane.gridy = 0;
+				panel.add(scrollPane, gbc_scrollPane);
+				{
+					notentabelle = new JTable();
+					model.addColumn("Fach");
+					model.addColumn("mündlich");
+					model.addColumn("schriftlich");
+					notentabelle.setModel(model);
+					scrollPane.setViewportView(notentabelle);
+				}
 			}
 		}
 		{
 			JPanel panel = new JPanel();
 			GridBagConstraints gbc_panel = new GridBagConstraints();
-			gbc_panel.insets = new Insets(0, 0, 5, 5);
+			gbc_panel.insets = new Insets(0, 0, 0, 5);
 			gbc_panel.fill = GridBagConstraints.BOTH;
 			gbc_panel.gridx = 1;
 			gbc_panel.gridy = 3;
 			contentPanel.add(panel, gbc_panel);
 			GridBagLayout gbl_panel = new GridBagLayout();
-			gbl_panel.columnWidths = new int[]{0, 0};
+			gbl_panel.columnWidths = new int[]{0, 0, 0};
 			gbl_panel.rowHeights = new int[]{0, 0};
-			gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+			gbl_panel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 			gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
 			panel.setLayout(gbl_panel);
 			{
 				JButton btnZurck = new JButton("Zur\u00FCck");
+				btnZurck.addActionListener((ActionListener) this);
 				GridBagConstraints gbc_btnZurck = new GridBagConstraints();
+				gbc_btnZurck.insets = new Insets(0, 0, 0, 5);
 				gbc_btnZurck.fill = GridBagConstraints.HORIZONTAL;
 				gbc_btnZurck.gridx = 0;
 				gbc_btnZurck.gridy = 0;
 				panel.add(btnZurck, gbc_btnZurck);
 			}
+			{
+				JButton btnNotenblattDrucken = new JButton("Notenblatt drucken");
+				GridBagConstraints gbc_btnNotenblattDrucken = new GridBagConstraints();
+				gbc_btnNotenblattDrucken.fill = GridBagConstraints.HORIZONTAL;
+				gbc_btnNotenblattDrucken.gridx = 1;
+				gbc_btnNotenblattDrucken.gridy = 0;
+				panel.add(btnNotenblattDrucken, gbc_btnNotenblattDrucken);
+			}
 		}
+	}
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+		String action = e.getActionCommand();
+		if(action.equals("Zurück"))
+		{
+			
+		}
+	}
+	
+	private void setDatenInMaske()
+	{
+		this.txt_schueler.setText(this.schueler.getNachname()+" "+this.schueler.getVorname());
+		this.txt_klasse.setText(this.schueler.getKlasseid().getBez());
+		this.txt_lehrer.setText(this.schueler.getKlasseid().getIdKlassenleiter().getNachname()+ " " + this.schueler.getKlasseid().getIdKlassenleiter().getVorname());
 	}
 
 }
