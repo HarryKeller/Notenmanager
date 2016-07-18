0b7d296683a5c9b728e54c380ef4f453663f0d50
Tim <tim-stroessner@t-online.de>
2016-07-18 11:21:56 +0200

    Notenblatt anpassung

--- src/Dialog/Dialog_Druckansicht.java
+++ src/Dialog/Dialog_Druckansicht.java
@@ -82,10 +82,12 @@ public class Dialog_Druckansicht extends JFrame
 			ArrayList<Map<String, ?>> al = new ArrayList<Map<String, ?>>();
 			//Einzelne Strings für jede mögliche Note anlegen
 			String mündl1j = "";
-			String mündl2j = "";
 			String schula1j = "";
+			String mündl2j = "";
 			String schula2j = "";
 		    Zeugnisnote zn = new Zeugnisnote(notenblatt.schueler);
+		    Zeugnisnote zzn = new Zeugnisnote(notenblatt.schueler);
+		    double zz = 0.00;
 		    double jz = 0.00;
 				for(Unterrichtsfach f : this.fach)
 				{
@@ -98,9 +100,14 @@ public class Dialog_Druckansicht extends JFrame
 					HashMap<String, String> hm = new HashMap<String, String>();
 					hm.put("Fach", f.getBez());
 					jz = zn.berechneNote(f);
+					zz = zzn.berechneZZNote(f);
 					if(jz!=0.00)
 					{
 						hm.put("jz", ""+zn.berechneNote(f));
+					}
+					if(zz!=0.00)
+					{
+						hm.put("zz", ""+zzn.berechneZZNote(f));
 					}
 					for(Leistung l : Leistung.AlleLesen(this.notenblatt.schueler, f))
 					{
@@ -111,22 +118,22 @@ public class Dialog_Druckansicht extends JFrame
 							{
 								if(l.getLeistungsart().getGewichtung() == 1)
 								{
-									mündl1j = mündl1j + " | " + l.getNotenstufe() + " | ";
+									mündl1j = mündl1j + "|" + l.getNotenstufe() + "|";
 								}
 								else if(l.getLeistungsart().getGewichtung() == 2)
 								{
-									schula1j = schula1j + " | " + l.getNotenstufe() + " | ";
+									schula1j = schula1j + "|" + l.getNotenstufe() + "|";
 								}
 							}
 							else if(l.getErhebungsdatum().isAfter(Dialog_Notenblatt.BEGINN_HALBJAHR))
 							{
 								if(l.getLeistungsart().getGewichtung() == 1)
 								{
-									mündl2j = mündl2j + " | " + l.getNotenstufe() + " | ";
+									mündl2j = mündl2j + "|" + l.getNotenstufe() + "|";
 								}
 								else if(l.getLeistungsart().getGewichtung() == 2)
 								{
-									schula2j = schula2j + " | " + l.getNotenstufe() + " | ";
+									schula2j = schula2j + "|" + l.getNotenstufe() + "|";
 								}
 							}
 						}
