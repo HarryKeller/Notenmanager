fdb114673eb99beddc761a427dadefb1d0848c52
Harry Keller <Harry Keller>
2016-07-26 14:57:45 +0200

    Fenster schliest sich

--- src/Dialog/Dialog_ZeugnisBemerkung.java
+++ src/Dialog/Dialog_ZeugnisBemerkung.java
@@ -54,7 +54,7 @@ public class Dialog_ZeugnisBemerkung extends JFrame implements ActionListener
 		initGUI();
 	}
 	private void initGUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 617, 439);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
