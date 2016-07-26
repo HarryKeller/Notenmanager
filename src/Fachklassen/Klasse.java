5f636eb7498a7bcb71911e0846133ff201505461
Patrick Schamel <Patrick Schamel>
2016-07-26 09:19:13 +0200

    Leeren von Warnungen in den Fachklassen

--- src/Fachklassen/Klasse.java
+++ src/Fachklassen/Klasse.java
@@ -2,7 +2,6 @@ package Fachklassen;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
--- src/Fachklassen/Schueler.java
+++ src/Fachklassen/Schueler.java
@@ -9,9 +9,6 @@ import java.util.Set;
 
 import javax.persistence.*;
 
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
 import Persistenz.DBZugriff;
 
 @Entity
