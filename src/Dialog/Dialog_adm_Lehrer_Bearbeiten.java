7117b7e5912830bdbe8a329494448a34502207b8
Harry Keller <Harry Keller>
2016-07-28 10:22:13 +0200

    Bermerkung geändert

--- src/Dialog/Dialog_adm_Lehrer_Bearbeiten.java
+++ src/Dialog/Dialog_adm_Lehrer_Bearbeiten.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
